f4f0120fd5dd8b9ab2ffbde20e91b57269cc9b4f
antag0n1st <trbogazov@gmail.com>
2016-10-12 12:34:03 +0200

    - improve ticker - no sounds - bitmap font

--- system/game.js
+++ system/game.js
@@ -64,7 +64,7 @@
                 Localization.instance().load();
 
                 ////////////////////////////////////////////////////////
-                var screen = new FontScreen(); // initial screen
+                var screen = new MainScreen(); // initial screen
                 ////////////////////////////////////////////////////////
 
                 game.navigator.add(screen, null, 200, function () {
